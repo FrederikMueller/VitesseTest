777b12b77487d036e45cb41cc128a6bf1a0a98a7
Fred <fmaxgalaxy@gmail.com>
2023-11-28 22:49:15 +0100

    Moving files to bench and adjusting layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <v-app>
-
-    <router-view></router-view>
-    </v-app>
+    <v-layout class="rounded rounded-md">
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <index></index>
+      </v-main>
+    </v-layout>
 </template>
+<script setup lang="ts">
+import Index from "~/pages/index.vue";
+</script>
